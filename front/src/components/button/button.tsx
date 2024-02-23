bf6f68e14274d747f5af802aefc06f9dded34ec4
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-02-23 21:37:37 +0300

    first merged kiady

--- front/src/components/button/button.tsx
+++ front/src/components/button/button.tsx
@@ -1,48 +1,48 @@
-import React, { PropsWithChildren } from "react";
-import "./button.css";
-import { AlertVariant } from "../../common.type";
-import classNames from "classnames";
+import React, { PropsWithChildren } from 'react';
+import './button.css';
+import { AlertVariant } from '../../common.type';
+import classNames from 'classnames';
 
 export function Button({
-    children,
-    variant = "primary",
-    outline = false,
-    onClick,
-    disabled = false,
-    className,
-    size = "md",
-    iconLeft,
-    ...props
+  children,
+  variant = 'primary',
+  outline = false,
+  onClick,
+  disabled = false,
+  className,
+  size = 'md',
+  iconLeft,
+  ...props
 }: ButtonProps) {
-    return (
-        <button
-            className={classNames(
-                "btn",
-                `btn-${variant}`,
-                size,
-                {
-                    outline,
-                    disabled,
-                },
-                className
-            )}
-            onClick={onClick}
-            disabled={disabled}
-            {...props}
-        >
-            {iconLeft}
-            {children}
-        </button>
-    );
+  return (
+    <button
+      className={classNames(
+        'btn',
+        `btn-${variant}`,
+        size,
+        {
+          outline,
+          disabled,
+        },
+        className,
+      )}
+      onClick={onClick}
+      disabled={disabled}
+      {...props}
+    >
+      {iconLeft}
+      {children}
+    </button>
+  );
 }
 
 type ButtonProps = PropsWithChildren<{
-    variant?: AlertVariant | "dark-primary";
-    size?: "md" | "sm";
-    outline?: boolean;
-    onClick?: () => void;
-    disabled?: boolean;
-    className?: string;
-    iconLeft?: React.ReactElement;
-    "data-cy"?: string;
+  variant?: AlertVariant | 'dark-primary';
+  size?: 'lg' | 'md' | 'sm';
+  outline?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
+  iconLeft?: React.ReactElement;
+  'data-cy'?: string;
 }>;
--- front/src/home/home.tsx
+++ front/src/home/home.tsx
@@ -11,52 +11,61 @@ export function Home() {
   const [showLoading, setShowLoading] = useState(true);
 
   useEffect(() => {
-      // Masquer l'élément loadingWrap après 300 millisecondes
-      const timer = setTimeout(() => {
-          setShowLoading(false);
-      }, 2100);
+    // Masquer l'élément loadingWrap après 300 millisecondes
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 2100);
 
-      // Nettoyer le timer lorsque le composant est démonté
-      return () => clearTimeout(timer);
+    // Nettoyer le timer lorsque le composant est démonté
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <AuthenticatedGuard>
-       {showLoading && (
-                <div id="loadingWrap">
-                    <div id="loadingIcon"></div>
-                </div>
-            )}
+      {showLoading && (
+        <div id="loadingWrap">
+          <div id="loadingIcon"></div>
+        </div>
+      )}
 
       <div className="contenu">
-        <div className='top-nav'>
-        <div className="logo"><p className='nameLogo'><span className='maka'>Maka</span>Fako</p></div>
+        <div className="top-nav">
+          <div className="logo">
+            <p className="nameLogo">
+              <span className="maka">Maka</span>Fako
+            </p>
+          </div>
 
-        <Button className='login'>Connecter</Button>
+          <Button className="login" size="lg">
+            Connecter
+          </Button>
         </div>
 
         <div className="paragraphe">
-          <H1 className='titre'>NETTOYONS NOS RUES, <br />protégeons notre planète.</H1>
+          <H1 className="titre">
+            NETTOYONS NOS RUES, <br />
+            protégeons notre planète.
+          </H1>
           <br />
           <br />
           <span className="tirer"></span>
           <br />
-          <H6 className='paraKely'>Ensemble, rendons notre monde plus propre et plus vert <br /> Unis dans notre engagement pour une terre plus propre.</H6>
+          <H6 className="paraKely">
+            Ensemble, rendons notre monde plus propre et plus vert <br /> Unis
+            dans notre engagement pour une terre plus propre.
+          </H6>
         </div>
 
         <div className="btnAlert">
-          <Button variant='primary' >Alerter</Button>
+          <Button variant="primary">Alerter</Button>
         </div>
 
         <div className="statistique">
           <H3 className="titreState">Nos state effectuer </H3>
 
-          <div className="NombreCamion">
-            
-          </div>
+          <div className="NombreCamion"></div>
         </div>
       </div>
-      
     </AuthenticatedGuard>
   );
 }
