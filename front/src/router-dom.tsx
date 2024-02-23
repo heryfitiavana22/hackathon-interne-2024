92cc36790b2355ea65916d236f092edd0c1efb64
josia08 <fanevampifalianaravalison@gmail.com>
2024-02-23 19:59:56 +0100

    first commit

--- front/src/router-dom.tsx
+++ front/src/router-dom.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Login } from './auth/login/login';
 import { Home } from './home/home';
+import { Payment } from './payment/Payment';
 
 const router = createBrowserRouter([
   { path: '/', element: <Home /> },
@@ -8,6 +9,11 @@ const router = createBrowserRouter([
     path: 'login',
     element: <Login />,
   },
+  {
+    path:'payment',
+    element:<Payment/>
+  }
+ 
 ]);
 export function RouterDom() {
   return <RouterProvider router={router} />;
