555f6acd86942fcf1aa31c0fa92dda6004cd41cc
rsjzoe <rsjzoe@gmail.com>
2024-02-23 22:42:56 +0300

    login page

--- front/src/auth/login/login.tsx
+++ front/src/auth/login/login.tsx
@@ -1,44 +1,60 @@
 import { PropsWithChildren } from 'react';
 import { useLogin } from './use-login';
 import { GuestGuard } from '../../components/guard/guest-guard';
+import './login.css';
 
 export function Login({}: LoginProps) {
   const { onSubmit, register } = useLogin();
+  // const [isCreate, setCreate] = useState(false);
 
   return (
-    <GuestGuard>
-      <div>
-        <div>
-          <h2>re-jwt</h2>
-        </div>
+    <div className="Login">
+      <div className="container">
+        <GuestGuard>
+          <div className="form-container sign-in-container">
+            <form method="post" onSubmit={onSubmit()}>
+              <h2>Se connecter</h2>
+              <span className='text'>Veuillez remplir les champs</span>
+              <div className="inputs">
+                {/* <label htmlFor="pseudo">Pseudo:</label> */}
+                <div className="input-container">
+                  <img className="icon" src="mail.svg" />
+                  <input
+                    type="text"
+                    id="pseudo"
+                    className="input"
+                    placeholder="herydj"
+                    {...register('pseudo', { required: true })}
+                  />
+                </div>
+                <div className="input-container">
+                  <img className="icon" src="lock.svg" />
+                  <input
+                    type="password"
+                    id="password"
+                    className="input"
+                    placeholder="mot de passe"
+                    {...register('password', { required: true })}
+                  />
+                </div>
+              </div>
 
-        <div>
-          <form method="post" onSubmit={onSubmit()}>
-            <div>
-              <label htmlFor="pseudo">pseudo :</label>
-              <input
-                type="text"
-                id="pseudo"
-                placeholder="herydj"
-                {...register('pseudo', { required: true })}
-              />
+              <div className="btn-connect">
+                <button>Se connecter</button>
+              </div>
+            </form>
+            {/* </div> */}
+          </div>
+        </GuestGuard>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-right">
+              <img src="./image/trash.jpg" alt="" className="trash" />
             </div>
-            <div>
-              <label htmlFor="password">mot de passe :</label>
-              <input
-                type="password"
-                id="password"
-                {...register('password', { required: true })}
-              />
-            </div>
-
-            <div>
-              <button>Se connecter</button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
-    </GuestGuard>
+    </div>
   );
 }
 
